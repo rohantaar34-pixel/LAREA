29ba3e0aea8902b5db860df53dfd540a5523d292
Zerohan_ACT2 <rohantaar34@gmail.com>
2025-11-29 16:05:56 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'Myapp',
+  appId: 'online.regionalhomeforgirls.larea',
+  appName: 'Regional Home',
   webDir: 'www'
 };
 
